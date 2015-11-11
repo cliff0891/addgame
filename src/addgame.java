b78145ef84b62b34695c1facbeddc7a98b91b4e9
cliff0891 <yuyunc@student.swosu.edu>
2015-11-11 09:51:15 -0600

    added a line

--- src/addgame.java
+++ src/addgame.java
@@ -11,15 +11,41 @@ import java.util.Scanner;
 		System.out.println("Inside the addition game method.");
 		
 		int hardness = 5;
+		int hardnessStep = 2;
+		int score = 0;
 		
 		// Set up my for loop to go through the number of rounds
-		int numberOfRounds = 2;
+		int numberOfRounds = 3;
 		for(int roundNumber = 1; 
 		roundNumber <= numberOfRounds;  
 		roundNumber = roundNumber + 1){
 			System.out.println("Inside the for loop. Round: " + roundNumber);
 			boolean isAnswerCorrect = getAndCheckStudentAnswer(hardness);
+			if(isAnswerCorrect){
+				System.out.print("Your score was " + score + " and is now ");
+				score = score + hardness;
+				System.out.println(score + ".");
+				
+				if(roundNumber<numberOfRounds){
+					System.out.print("Your hardness was " + hardness + " and is now ");
+					hardness = hardness * hardnessStep;
+					System.out.println(hardness + ".");
+				}
+			}else{
+				if(roundNumber<numberOfRounds){
+					System.out.print("Your hardness was " + hardness + " and is now ");
+					if(hardness>5){
+						hardness = hardness / hardnessStep;
+						
+					}
+					System.out.println(hardness + ".");
+					
+				}
+				
+			}
 		}
+		System.out.println("The game is complete.");
+		System.out.println("Your final score was " + score );
 	}
 	
 	public static boolean getAndCheckStudentAnswer(int hardness) {
