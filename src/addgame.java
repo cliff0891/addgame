3b7d83713196df992cfc0622f0395a20a2836993
cliff0891 <yuyunc@student.swosu.edu>
2015-11-11 09:23:07 -0600

    new line of code

--- src/addgame.java
+++ src/addgame.java
@@ -1,11 +1,20 @@
 
 public class addgame {
-
-	
-	//call the addition game method.
-	
+	public static void main(String[] args) {
+		System.out.println("Hello class.");
 		
-			
+		//Call the addition game method.
+		AdditonGameMethod();
 	}
-
-
+	public static void AdditonGameMethod() {
+		System.out.println("Inside the addition game method.");
+		
+		// Set up my for loop to go through the number of rounds
+		int numberOfRounds = 2;
+		for(int roundNumber = 1; 
+		roundNumber <= numberOfRounds;  
+		roundNumber = roundNumber + 1){
+			System.out.println("Inside the for loop. Round: " + roundNumber);
+		}
+	}
+}
